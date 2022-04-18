84e19bfc5b235897458427108a8909bc3e88403c
sdshuvo98 <hellosdshuvo@gamil.com>
2022-04-18 15:55:00 +0600

    image added

--- src/Pages/Home/HomeHeader/HomeHeader.js
+++ src/Pages/Home/HomeHeader/HomeHeader.js
@@ -6,35 +6,32 @@ const HomeHeader = () => {
             <div className="carousel-inner relative w-full overflow-hidden">
                 <div className="carousel-item active relative float-left w-full">
                     <img
-                        src="https://mdbcdn.b-cdn.net/img/new/slides/041.webp"
+                        src="https://i.ibb.co/BPc3cZ5/carousel-1.jpg"
                         className="block w-full rounded-3xl"
                         alt="Wild Landscape"
                     />
                     <div className="carousel-caption hidden md:block absolute top-52 text-center">
-                        <h5 className="text-4xl">First slide label</h5>
-                        <p>Some representative placeholder content for the first slide.</p>
+                        <h5 className="text-4xl font-semibold">Enjoy your Best Travel with Us!</h5>
                     </div>
                 </div>
                 <div className="carousel-item relative float-left w-full">
                     <img
-                        src="https://mdbcdn.b-cdn.net/img/new/slides/042.webp"
+                        src="https://i.ibb.co/p4SK5GD/carousel-2.jpg"
                         className="block w-full rounded-3xl"
                         alt="Camera"
                     />
                     <div className="carousel-caption hidden md:block absolute top-52 text-center">
-                        <h5 className="text-4xl">First slide label</h5>
-                        <p>Some representative placeholder content for the first slide.</p>
+                        <h5 className="text-4xl font-semibold">Explore the World with Traveeel!</h5>
                     </div>
                 </div>
                 <div className="carousel-item relative float-left w-full">
                     <img
-                        src="https://mdbcdn.b-cdn.net/img/new/slides/043.webp"
+                        src="https://i.ibb.co/41ny8nY/carousel-3.jpg"
                         className="block w-full rounded-3xl"
                         alt="Exotic Fruits"
                     />
                     <div className="carousel-caption hidden md:block absolute top-52 text-center">
-                        <h5 className="text-4xl">First slide label</h5>
-                        <p>Some representative placeholder content for the first slide.</p>
+                        <h5 className="text-4xl font-semibold">The Best Reliable Choice for every Traveler.</h5>
                     </div>
                 </div>
             </div>
